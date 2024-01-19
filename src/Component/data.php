dd2f089c9933b2520d54cc46358aaf521b8a5eaa
emred <emredogan@hotmail.com>
2024-01-19 12:31:44 +0300

    Fix undefined index on SetTaxExemption

--- src/Component/data.php
+++ src/Component/data.php
@@ -273,13 +273,7 @@ class data
 
     public function setTaxExemption($data = array())
     {
-        $taxSubtotalIndex = $data["TaxTypeCode"] . $data["Percent"];
-
-        if (!isset($this->data["cac:TaxTotal"]["cac:TaxSubtotal"][$taxSubtotalIndex])) {
-            $this->data["cac:TaxTotal"]["cac:TaxSubtotal"][$taxSubtotalIndex] = array();
-        }
-
-        $this->data["cac:TaxTotal"]["cac:TaxSubtotal"][$taxSubtotalIndex]["cac:TaxCategory"] += array(
+        $this->data["cac:TaxTotal"]["cac:TaxSubtotal"][@$data["TaxTypeCode"].@$data["Percent"]]["cac:TaxCategory"] += array(
             "cbc:TaxExemptionReasonCode" => $data["TaxExemptionReasonCode"],
             "cbc:TaxExemptionReason" => $data["TaxExemptionReason"],
         );
